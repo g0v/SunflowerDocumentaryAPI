6735cf2d5eba27d5262c03e2219c5cc69142c3a2
Lee <jessy1092@gmail.com>
2014-04-28 02:14:56 +0800

    Add event's name in eventsData - add api/1.0b for testing.
    Signed-off-by: Lee <jessy1092@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express        = require('express');
 var app            = express();
 
+var db = require('./lib/transferJSON')
+
 var eventsData = require('./data/eventsData');
 var timelineData = require('./data/timelineData');
 var tagsData = require('./data/tagsData');
 
 var v = '1.0';
+var beta = '1.0b';
+
+db.init();
 
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -15,6 +20,10 @@ app.all('/*', function(req, res, next) {
 
 app.get('/api/' + v + '/events', function (req, res) {
     res.send(eventsData);
+});
+
+app.get('/api/' + beta + '/events', function (req, res) {
+    res.send(db.getEvents());
 });
 
 app.get('/api/' + v + '/timeline', function (req, res) {
