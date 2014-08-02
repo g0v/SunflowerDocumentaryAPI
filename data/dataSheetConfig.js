7b770d0d29ed2d51bd596aec7a9384585e877e0d
Lee <jessy1092@gmail.com>
2014-08-02 16:46:53 +0800

    Add 4 month Data.

--- data/dataSheetConfig.js
+++ data/dataSheetConfig.js
@@ -1,21 +1,13 @@
 module.exports = {
     host: 'spreadsheets.google.com',
-    sheetID: '1enjDLyGzghZt2p27ZG3kaEt2bRxdGzpQ9bEvKmombr4',
-    listID: [
-        "od6",
-        "2006457055",
-        "252412096",
-        "87303709",
-        "1759904441",
-        "177806334",
-        "272166176",
-        "1407017554",
-        "373459406",
-        "2014635305",
-        "897131457",
-        "1934885794",
-        "230474246",
-        "471107307",
-        "1084489457"
+    sheetArray: [
+        {
+            sheetID: '1enjDLyGzghZt2p27ZG3kaEt2bRxdGzpQ9bEvKmombr4',
+            listNum: 15
+        },
+        {
+            sheetID: '1-Nz9Z2wOrHKICF75ur8QHLHBUTkHw2E4_jZpmd9UiBk',
+            listNum: 29
+        }
     ]
 }
